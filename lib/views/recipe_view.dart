7bafa5b1ebfb46e63d9df5fb566d301c739bb3d1
Nikhil Gupta <ngupta8700764@gmail.com>
2020-09-30 01:13:47 +0530

    // added

--- lib/views/recipe_view.dart
+++ lib/views/recipe_view.dart
@@ -63,6 +63,7 @@ class _RecipeViewState extends State<RecipeView> {
                     ),
                     Text(
                       "Recipes",
+                      //
                       style: TextStyle(
                           fontSize: 18,
                           color: Colors.blue,
